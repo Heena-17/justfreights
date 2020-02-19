0a6093ad688b37f568daa1196c6af467662fcb5c
Heena-17 <heenu76667@gmail.com>
2020-02-19 11:43:39 +0530

    nothing

--- Api.php
+++ Api.php
@@ -1,33 +1,15 @@
 <?php
 
 namespace app\models;
+use yii\base\Model;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+class Api extends \yii\db\ActiveRecord 
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
-
+   
+public static function tableName()   
+    {   
+        return 'student';   
+    } 
     /**
      * {@inheritdoc}
      */
